0420e610ba989861cc2dab459ab5f0f146fbd920
James Hunt <jameshuntwork1997@yahoo.com>
2017-11-02 11:05:44 +0000

    Stage 1 - File Prep
    In this commit I have set up the classes I will need and arranged the GUI so that the user has a clear vision of the game that they will be playing.

---
+++ PongGame/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace PongGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picPongGame = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.lblHeader = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picPongGame)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picPongGame
+            // 
+            this.picPongGame.Location = new System.Drawing.Point(13, 65);
+            this.picPongGame.Name = "picPongGame";
+            this.picPongGame.Size = new System.Drawing.Size(1132, 566);
+            this.picPongGame.TabIndex = 0;
+            this.picPongGame.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 46);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(233, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(104, 46);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "STOP";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(123, 13);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(104, 46);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "PAUSE";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(343, 13);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(104, 46);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "RESTART";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // lblHeader
+            // 
+            this.lblHeader.AutoSize = true;
+            this.lblHeader.Font = new System.Drawing.Font("Candara", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeader.Location = new System.Drawing.Point(828, 7);
+            this.lblHeader.Name = "lblHeader";
+            this.lblHeader.Size = new System.Drawing.Size(317, 33);
+            this.lblHeader.TabIndex = 5;
+            this.lblHeader.Text = "WELCOME TO PONG IN C#";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(887, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(258, 19);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Designed and created by James Hunt";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1157, 643);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblHeader);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.picPongGame);
+            this.Name = "Form1";
+            this.Text = "Pong - James Hunt";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picPongGame)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picPongGame;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label lblHeader;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
